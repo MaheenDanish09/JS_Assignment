446033b4dc1b6fdbae85c827964f9a1fe27bb0a5
MaheenDanish09 <mariammaheen000@gmail.com>
2026-02-12 00:35:36 +0500

    JS Practice Assignements

--- chapter5.js
+++ chapter5.js
@@ -1,3 +1,47 @@
 //////////MATH EXPRESSIONS//////////
 
 // QUESTION 1 //
+// var num1 = 5;
+// var num2 = 10;
+// var total = num1 + num2;
+// document.writeln("Sum of 5 and 10 is " + total);
+
+// QUESTION 2 //
+//////////// SUBTRACTION
+// var subtraction = num1 - num2;
+// document.writeln("Subtraction: " + subtraction);
+//////////// MULTIPLICATION
+// var multiplication = num1*num2;
+// document.writeln("Multiplication: " + multiplication);
+//////////// DIVISION
+// var division = num1/num2;
+// document.writeln("Division: " + division);
+//////////// MODULUS
+// var modulus = num1%num2;
+// document.writeln("Modulus: " + modulus);
+
+// QUESTION 3 //
+// var num;
+// document.writeln("Value after variable declaration is " + num);
+// num = 20;
+// document.writeln("Initial value:" + num);
+// num++;
+// document.writeln("Value after increment is: " + num);
+// num = 21 + 7;
+// document.writeln("Value after addition is: " + num);
+// num--;
+// document.writeln("Value after decrement is: " + num);
+// num = 27%3;
+// document.writeln("The remainder is: " + num);
+
+// QUESTION 4 //
+// var ticketPrice = 600;
+// document.writeln("Total cost to buy 5 tickets to a movie is " + ticketPrice*5 + "PKR");
+
+// QUESTION 5 //
+// for(let i=1; i<=10; i++){
+// document.writeln(`5 x ${i} = ${5*i} <br>`);
+// };
+
+// QUESTION 6 //
+
