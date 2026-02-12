0bfbca95a7246df4daf8e750bc4c467c0315ec20
MaheenDanish09 <mariammaheen000@gmail.com>
2026-02-12 11:49:29 +0500

    JS Practice Assignments

--- chapter5.js
+++ chapter5.js
@@ -44,4 +44,59 @@
 // };
 
 // QUESTION 6 //
+////////////CELSIUS TO FAHRENHEIT
+// var celsius = 25;
+// var fahrenheit = (celsius * 9/5) + 32;
+// document.writeln(celsius + "°C is " + fahrenheit +"°F <br>");
+////////////FAHRENHEIT TO CELSIUS
+// var fahrenheitTemp = 70;
+// var celsiusTemp = (fahrenheitTemp - 32) *5/9;
+// document.writeln(fahrenheitTemp + "°F is " + celsiusTemp + "°C");
 
+// QUESTION 7 //
+////////////PRICES
+// var price1 = 650;
+// var price2 = 100;
+////////////QUANTITIES
+// var quantity1 = 3;
+// var quantity2 = 7;
+////////////SHIPPING CHARGES
+// var charges = 100;
+////////////CALCULATIONS
+// var totalCost = (price1 * quantity1) + (price2 * quantity2) + charges;
+// document.writeln("<h1>Shopping cart</h1>")
+// document.writeln("Total cost of your order is " + totalCost);
+
+// QUESTION 8 //
+// var totalMarks = 980;
+// var obtainedMarks = 804;
+// var percentage = (obtainedMarks / totalMarks) * 100;
+// document.writeln("<h1>Marks Sheet</h1>");
+// document.writeln("Total Marks: " + totalMarks + "<br>");
+// document.writeln("Marks Obtained: " + obtainedMarks + "<br>");
+// document.writeln("Percentage: " + percentage + "%");
+
+// QUESTION 9 //
+// document.writeln("<h1>Currency in PKR</h1>")
+// document.writeln("Total Currency in PKR: " + ((10 * 104.80) + (25 * 28)));
+
+// QUESTION 10 //
+// var num = 3;
+// document.writeln((num + 5) * 10 / 2);
+
+// QUESTION 11 //
+// var currentYear = 2026;
+// var birthYear = 2009;
+// document.writeln("<h1>Age Calculator</h1> <br>" + "Current Year: " + currentYear +"<br>" + "Birth Year: " + birthYear +"<br>"+ "Your Age is: " + (currentYear - birthYear));
+
+// QUESTION 12 //
+// var radius = 20;
+// document.writeln("<h1>The Geometrizer</h1> <br>" + "Radius of a circle: " + radius +"<br>" + "The circumference is: " + (2*3.142*radius) +"<br>"+ "The area is: " + (3.142*(radius*radius)));
+
+// QUESTION 13 //
+// var favSnack = "chips";
+// var age = 16;
+// var maxAge = 70;
+// var snacksPerDay = 3;
+// document.writeln("<h1>The Lifetime Supply Calculator</h1> <br>" + "Favorite Snack: " + favSnack +"<br>"+ "Current Age: " + age +"<br>"+ "Estimated Maximum Age: " + maxAge +"<br>"+ "Amount of snacks per day: " + snacksPerDay +"<br>");
+// document.writeln("You will need " + ((maxAge - age) * snacksPerDay + favSnack) + " to last you until the ripe old age of " + maxAge);
