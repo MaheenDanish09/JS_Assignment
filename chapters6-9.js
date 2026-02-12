d11f69d6c11661b6cee5e62c3b9139e5365decec
MaheenDanish09 <mariammaheen000@gmail.com>
2026-02-12 19:21:11 +0500

    JS Practice Assignments

---
+++ chapters6-9.js
@@ -0,0 +1,39 @@
+//////////MATH EXPRESSIONS//////////
+
+// QUESTION 1 //
+// let a = 10;
+// document.writeln("Result: <br>")
+// document.writeln("The value of a is: " + a +"<br><br>");
+// ++a;
+// document.writeln("The value of ++a is: " + a +"<br>"+"Now the value of a is: " + a +"<br><br>");
+// a++;
+// document.writeln("The value of a++ is: " + a +"<br>"+"Now the value of a is: " + a+"<br><br>");
+// --a;
+// document.writeln("The value of --a is: " + a +"<br>" + "Now the value of a is: " + a +"<br><br>");
+// a--;
+// document.writeln("The value of a-- is: " + a +"<br>" + "Now the value of a is: " + a );
+
+// QUESTION 2 //
+// let a = 2;////1
+// let b = 1;////- 0 + 1 + 1
+// let result = --a - --b + ++b + b--; ////1 - 0 + 1 + 1
+// document.writeln("a = " + a +"<br>"+ "b = " + b +"<br>" + "result = " + result);
+
+// QUESTION 3 //
+// var input = prompt("Enter your name...");
+// if(input){
+// var greeting= "Hello, " + input + "! Welcome to the site.";
+// alert(greeting);
+// };
+
+// QUESTION 4 //
+// let table = +prompt("Enter a number!",5);
+// for(let i=1; i<=10; i++){
+//     document.writeln(`${table} x ${i} = ${table*i} <br>`);
+// };
+
+// QUESTION 5 //
+// let subject1 = prompt("Enter Subject 1 ");
+// let subject2 = prompt("Enter Subject 2");
+// let subject3 = prompt("Enter Subject 3");
+// let marks = 100;
---
+++ chapters9-11.js
@@ -0,0 +1,33 @@
+//////////USER INPUT & CONDITIONAL STATEMENT//////////
+
+// QUESTION 1 //
+// var userCity = prompt("Enter your city!");
+// var city = "Karachi".toLowerCase();
+// if(userCity === city){
+// alert("Welcome to city of lights!");
+// }else{
+//     alert("Welcome");
+// };
+
+// QUESTION 2 //
+// var userGender = prompt("Enter your gender");
+// var gender = "Male".toLowerCase();
+// if(userGender === gender){
+// alert("Good Morning Sir");
+// }else{
+//     alert("Good morning Ma'am");
+// };
+
+// QUESTION 3 //
+// var color = prompt("Enter traffic signal color (Red, Yellow, Green):");
+// color = color.toLowerCase();
+// if (color === "red") {
+//     alert("MUST STOP!");
+// }else if (color === "yellow") {
+//     alert("Ready To Move");
+// }else if (color === "green") {
+//     alert("Move Now");
+// };
+
+// QUESTION 4 //
+
